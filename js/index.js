9e45bab94045db1d73393ed28cb7251ea2c18fcf
jeay <wrjie@msn.cn>
2016-12-03 10:36:08 +0800

    section b

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-$(){
-	$(".b-bg")
-}
+$(function () {
+	/*section b*/
+	$(".b-bg").hide();
+	$(".b-bg").eq(0).show();
+	var len=$(".b-bg").length;
+	i=0;
+	$(".b-b .icon-angle-right").click(function() {
+		i++;
+		if (i==len) {i=0};
+		$(".b-bg").hide();
+		$(".b-bg").eq(i).show();
+	})
+	$(".b-b .icon-angle-left").click(function() {
+		if (i==0) {i=len};
+		i--;
+		$(".b-bg").hide();
+		$(".b-bg").eq(i).show();
+	})
+})
