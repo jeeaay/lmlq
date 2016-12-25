63276665ab7d30fb3e92a901cfdc68772f3740f5
jeay <wrjie@msn.cn>
2016-12-25 16:58:51 +0800

    新闻栏目封面页

---
+++ js/news.js
@@ -0,0 +1,35 @@
+$(function () {
+
+	/*new index carousel*/
+	var ch_active=function (i) {
+		$(".news_group .carou li").hide();
+		$(".news_group .carou li").eq(i).fadeIn(200);
+		$(".news_group_r li").removeClass("active");
+		$(".news_group_r li").eq(i).addClass("active");
+	}
+	$(".news_group .carou li").hide();
+	$(".news_group .carou li").first().show();
+	$(".news_group .pointer .l").click(function() {
+		var i=$(".news_group .carou li:visible").index();
+		if (i!=0) {
+			i--;
+		}else{
+			i=$(".news_group .carou li").length-1;
+		}
+		ch_active(i);
+	});
+	$(".news_group .pointer .r").click(function() {
+		var i=$(".news_group .carou li:visible").index();
+		if (i!=$(".news_group .carou li").length-1) {
+			i++;
+		}else{
+			i=0;
+		}
+		ch_active(i);
+	})
+	$(".news_group_r li").mouseenter(function() {
+		ch_active($(this).index());
+	})
+
+	
+})
