4854e92dc1b00a90cc7a3a6a882a98b5270b2481
jeay <wrjie@msn.cn>
2016-12-27 16:08:05 +0800

    新闻频道首页

--- js/news_article.js
+++ js/news_article.js
@@ -1,18 +1,19 @@
 $(function () {
 	//set font size
-	current_font_size =parseFloat($(".cont p").css("font-size"),10);
+	var obj=$(".cont p");
+	current_font_size =parseFloat(obj.css("font-size"),10);
 	$(".font-bigger").click(function(){
 		current_font_size=current_font_size+1;
-		$(".cont p").css("font-size",  current_font_size + "px" );
+		obj.css("font-size",  current_font_size + "px" );
 	});
 	$(".font-normal").click(function(){
 		current_font_size=14;
-		$(".cont p").css("font-size",  current_font_size + "px" );
+		obj.css("font-size",  current_font_size + "px" );
 	});
 	$(".font-smaller").click(function(){
 		if (current_font_size>12) {
 			current_font_size=current_font_size-1;
 		};
-		$(".cont p").css("font-size",  current_font_size + "px" );
+		obj.css("font-size",  current_font_size + "px" );
 	});	
 })
